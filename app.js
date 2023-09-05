46b2afa46a79a7faa4938eb98ff4bf3b1f2d28ae
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-09-05 07:17:06 +0200

    upload hw

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const db = require("./models/contactsdb");
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
+const contactsDBRouter = require("./routes/api/contactsdb");
 
-const app = express()
+const app = express();
+// const appdb = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+// const formatsLoggerdb = appdb.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+// appdb.use(logger(formatsLoggerdb));
+// appdb.use(cors());
+// appdb.use(express.json());
+
+app.use("/api/v1/contacts", contactsRouter);
+app.use("/api/v2/contacts", contactsDBRouter);
+
+app.use("/css", express.static(path.join(__dirname, "views/pages/css")));
+app.use("/js", express.static(path.join(__dirname, "views/pages/js")));
+app.use("/images", express.static(path.join(__dirname, "views/pages/images")));
+
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  res.render("./pages/index");
+});
+
+app.get("/uptime/db", function (req, res) {
+  res.json({
+    message: "DB status",
+    status: db.dbstatus(),
+    code: 200,
+  });
+});
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
+
+// appdb.use((req, res) => {
+//   res.status(404).json({ message: "Not found" });
+// });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+// appdb.use((err, req, res, next) => {
+//   res.status(500).json({ message: err.message });
+// });
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,52 @@
-const app = require('./app')
+const app = require("./app");
+// const appdb = require("./app");
+
+const db = require("./models/contactsdb");
+
+(async () => {
+  try {
+    await db.connectdb;
+    console.log(`Connected to database`);
+  } catch (err) {
+    console.log(`Connection to database faild: ${err.message}`);
+    return "error";
+  }
+})();
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+// appdb.listen(3001, () => {
+//   console.log("Server running. Use our API on port: 3001");
+// });
+
+const dbConnectionClose = function (msg, callback) {
+  db.disconnectdb();
+  console.log("Mongoose disconnected through " + msg);
+  callback();
+};
+// For nodemon restarts
+process.once("SIGUSR2", function () {
+  dbConnectionClose("nodemon restart", function () {
+    process.kill(process.pid, "SIGUSR2");
+  });
+});
+// For app termination
+process.on("SIGINT", function () {
+  dbConnectionClose("app termination", function () {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on("SIGTERM", function () {
+  dbConnectionClose("Heroku app termination", function () {
+    process.exit(0);
+  });
+});
+// For process exit
+process.on("exit", function () {
+  dbConnectionClose("process exit", function () {
+    process.exit(0);
+  });
+});
